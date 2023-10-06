762322033057e4eef33448c9853c6b6b8f7c41cc
cyborg-v1 <martincelis1221@gmail.com>
2023-10-06 12:28:53 -0300

    Aprendi: constante metadata

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import NavBar from "@/components/NavBar";
 
+export const metadata = {
+  title: "Aprendiendo NextJS",
+  description: "Pagina principal de la app"
+}
+
 export default function RootLayout({ children }) {
   return (
     <html>
--- app/tienda/categorias/computadoras/page.jsx
+++ app/tienda/categorias/computadoras/page.jsx
@@ -1,3 +1,6 @@
+export const metadata = {
+  title:"Pagina de computadoras"
+}
 
 function Computadoras() {
   return (
